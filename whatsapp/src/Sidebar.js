31efb391db3370efb0270d28e2aeab61fe60ef31
João Espinheira <joao.espinheira.1998@gmail.com>
2022-11-09 21:31:52 +0000

    firebase configs

--- whatsapp/src/Sidebar.js
+++ whatsapp/src/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Sidebar.css";
 import Avatar from "@mui/material/Avatar";
 import DonutLargeIcon from "@mui/icons-material/DonutLarge";
@@ -7,8 +7,22 @@ import MoreVertIcon from "@mui/icons-material/MoreVert";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
 import SidebarChat from "./SidebarChat";
+import db from "./firebase";
 
 function Sidebar() {
+  const [rooms, setRooms] = useState([]);
+
+  useEffect(() => {
+    db.collection("rooms").onSnapshot((snapshot) =>
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+  }, []);
+
   return (
     <div className="sidebar">
       <div className="sidebar__header">
